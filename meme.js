6cc1d1397452be2c481e0914814be33fb679b49a
sohailansari01 <sohailansari90786@gmail.com>
2024-09-02 02:45:21 +0530

    feat: Replace Meme API with Programming Memes API and implement random meme selection - Switched from wholesomememes API to Programming Memes API via XMLHttpRequest. - Updated the meme display logic to randomly select and display a meme from the API response.

--- meme.js
+++ meme.js
@@ -1,21 +1,36 @@
-const generateBtn = document.querySelector('.generate-button')
-const memeTitle = document.querySelector('.meme-title')
-const memeImage = document.querySelector('.meme-image')
-const authorOutput = document.querySelector('.author')
+const generateBtn = document.querySelector('.generate-button');
+const memeTitle = document.querySelector('.meme-title');
+const memeImage = document.querySelector('.meme-image');
+const authorOutput = document.querySelector('.author');
 
 function getMeme() {
-  fetch('https://meme-api.com/gimme/wholesomememes')
-    .then((res) => res.json())
-    .then((data) => {
-      const { author, title, url } = data
-      memeTitle.innerText = title
-      memeImage.src = url
-      authorOutput.innerText = `Meme by: ${author}`
-    })
+  const data = null;
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      const response = JSON.parse(this.responseText);
+      // Select a random meme from the array of memes
+      const randomIndex = Math.floor(Math.random() * response.length);
+      const meme = response[randomIndex];
+      const { id, image, text } = meme;
+
+      memeTitle.innerText = text || "Programming Meme";
+      memeImage.src = image;
+      authorOutput.innerText = `Meme ID: ${id}`;
+    }
+  });
+
+  xhr.open('GET', 'https://programming-memes-images.p.rapidapi.com/v1/memes');
+  xhr.setRequestHeader('x-rapidapi-key', 'fe73a01a4fmsh14957bd844f88d6p1e9ba4jsn1526717c90e8');
+  xhr.setRequestHeader('x-rapidapi-host', 'programming-memes-images.p.rapidapi.com');
+
+  xhr.send(data);
 }
 
-getMeme()
+getMeme();
 
 generateBtn.addEventListener('click', () => {
-  getMeme()
-})
+  getMeme();
+});
